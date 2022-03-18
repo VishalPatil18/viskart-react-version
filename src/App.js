addd3ab73716a19fd4ea254d72e9d67f5139ed61
VishalPatil18 <vishalpatil.imp@gmail.com>
2022-03-18 09:47:13 +0530

    FIX: Screen height fixed to make footer stick at bottom of viewport

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import styles from "./App.module.css";
 
 function App() {
   return (
-    <div className="App">
+    <div className={styles.app}>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home cname={styles.main} />} />
